ee404c3dd1399655ec2da38a5de50b2db3cb41fe
midCenturyDude7 <mgriffes@gmail.com>
2023-05-12 17:39:40 -0700

    Applied mouse event to psuedo element wrapper::after

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const wrapperAfter = document.querySelector(".wrapper");
+const wrapperElem = document.querySelector(".wrapper");
+const wrapperAfter = window.getComputedStyle(wrapperElem, "::after");
 const sectionBox = document.querySelector(".section-box");
+
+console.log(wrapperAfter.animation);
 
 sectionBox.addEventListener("mouseover", () => {
   console.log("mouseover!");
-  wrapperAfter.style.setProperty("--slide-02", "slide-01");
+  wrapperElem.style.setProperty(
+    "--slide-02",
+    "slide-down 1s ease-in-out backwards"
+  );
+});
+
+sectionBox.addEventListener("mouseout", () => {
+  console.log("mouseout!");
+  wrapperElem.style.setProperty(
+    "--slide-02",
+    "slide-up-bounce 1s ease-in forwards"
+  );
 });
