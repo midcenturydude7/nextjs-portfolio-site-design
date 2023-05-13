4606382e287956e9e2d8b57f2e9b73d14248ffab
midCenturyDude7 <mgriffes@gmail.com>
2023-05-12 19:05:02 -0700

    Applied hover event to footer animation

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
 const wrapperElem = document.querySelector(".wrapper");
 const sectionBox = document.querySelector(".section-box");
 const sectionBoxTwo = document.querySelector(".section-box:nth-child(2)");
+const footerElem = document.querySelector(".footer");
 
 // Testing access to CSS variables | Pseudo element - wrapper::after
 // const wrapperAfter = window.getComputedStyle(wrapperElem, "::after");
 
-console.log(sectionBoxTwo);
+console.log(footerElem);
 
 sectionBox.addEventListener("mouseover", () => {
-  console.log("mouseover!");
   wrapperElem.style.setProperty(
     "--slide-02",
     "slide-down 1s ease-in-out backwards"
+  );
+  footerElem.style.setProperty(
+    "--slide-03",
+    "slide-down-content 1s ease-in-out forwards"
   );
 });
 
 sectionBox.addEventListener("mouseout", () => {
-  console.log("mouseout!");
   wrapperElem.style.setProperty(
     "--slide-02",
     "slide-up-bounce 1s ease-in forwards"
+  );
+  footerElem.style.setProperty(
+    "--slide-03",
+    "slide-up-content 1s ease-in-out forwards"
   );
 });
 
 sectionBoxTwo.addEventListener("mouseover", () => {
-  console.log("mouseover!");
   wrapperElem.style.setProperty(
     "--slide-02",
     "slide-down 1s ease-in-out backwards"
   );
+  footerElem.style.setProperty(
+    "--slide-03",
+    "slide-down-content 1s ease-in-out forwards"
+  );
 });
 
 sectionBoxTwo.addEventListener("mouseout", () => {
-  console.log("mouseout!");
   wrapperElem.style.setProperty(
     "--slide-02",
     "slide-up-bounce 1s ease-in forwards"
+  );
+  footerElem.style.setProperty(
+    "--slide-03",
+    "slide-up-content 1s ease-in-out forwards"
   );
 });
