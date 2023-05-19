5dcac010311c2a5df0ee5149e81529740ea20ba1
midCenturyDude7 <mgriffes@gmail.com>
2023-05-19 12:15:31 -0700

    Finished terminal window

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const footerElem = document.querySelector(".footer");
 // Testing access to CSS variables | Pseudo element - wrapper::after
 // const wrapperAfter = window.getComputedStyle(wrapperElem, "::after");
 
-console.log(footerElem);
-
 // BOX ONE: MOUSEOVER
 sectionBox.addEventListener("pointerover", () => {
   wrapperElem.style.setProperty(
@@ -55,3 +53,59 @@ sectionBoxTwo.addEventListener("pointerout", () => {
     "slide-up-content 1s ease-in forwards"
   );
 });
+
+// TERMINAL WINDOW TEXT
+consoleText(
+  ["I build things for the web.", "Check out my featured projects below..."],
+  "text",
+  ["#00ff00", "#00ff00"]
+);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ["#fff"];
+  let visible = true;
+  const consoleText = document.getElementById("console-text");
+  let letterCount = 1;
+  let x = 1;
+  let waiting = false;
+  const target = document.getElementById(id);
+  target.setAttribute("style", "color:" + colors[0]);
+
+  window.setInterval(function () {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
+        let usedColor = colors.shift();
+        colors.push(usedColor);
+        let usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute("style", "color:" + colors[0]);
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function () {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 120);
+
+  window.setInterval(function () {
+    if (visible === true) {
+      consoleText.className = "console-underscore hidden";
+      visible = false;
+    } else {
+      consoleText.className = "console-underscore";
+
+      visible = true;
+    }
+  }, 400);
+}
